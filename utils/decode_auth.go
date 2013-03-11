35794c19308a4fd9da662f1fbc6aefb294dfdd74
Conor Hunt <conor.hunt+git@gmail.com>
2013-03-11 15:14:12 -0400

    tool parses ntlm v1 or v2

--- utils/decode_auth.go
+++ utils/decode_auth.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"encoding/base64"
+	"flag"
 	"fmt"
 	"ntlm/messages"
 )
 
 func main() {
+	var ntlmVersion = flag.Int("ntlm", 2, "NTLM version to try: 1 or 2")
+	flag.Parse()
 	var data string
-	fmt.Println("Paste the base64 encoded Authenticate message:")
+	fmt.Println("Paste the base64 encoded Authenticate message (with no line breaks):")
 	fmt.Scanf("%s", &data)
 	authenticateData, _ := base64.StdEncoding.DecodeString(data)
-	a, _ := messages.ParseAuthenticateMessage(authenticateData, 2)
+	a, _ := messages.ParseAuthenticateMessage(authenticateData, *ntlmVersion)
 	fmt.Printf(a.String())
 }
